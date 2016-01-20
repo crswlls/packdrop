07f45a69ada3af2433ce4e0d773d707f85955169
Chris Williams <chris.williams@mixrad.io>
2016-01-20 17:21:25 +0000

    Added comedy logo

--- PackDrop/Resources/Resource.designer.cs
+++ PackDrop/Resources/Resource.designer.cs
@@ -43,71 +43,90 @@ namespace PackDrop
 			}
 		}
 		
+		public partial class Drawable
+		{
+			
+			// aapt resource value: 0x7f020000
+			public const int logo = 2130837504;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050004
-			public const int Continue = 2131034116;
+			// aapt resource value: 0x7f060004
+			public const int Continue = 2131099652;
 			
-			// aapt resource value: 0x7f050000
-			public const int artworkPreview = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int artworkPreview = 2131099648;
 			
-			// aapt resource value: 0x7f050009
-			public const int downBtn = 2131034121;
+			// aapt resource value: 0x7f060009
+			public const int downBtn = 2131099657;
 			
-			// aapt resource value: 0x7f050002
-			public const int editText = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int editText = 2131099650;
 			
-			// aapt resource value: 0x7f050003
-			public const int gallery = 2131034115;
+			// aapt resource value: 0x7f060003
+			public const int gallery = 2131099651;
 			
-			// aapt resource value: 0x7f05000b
-			public const int game1 = 2131034123;
+			// aapt resource value: 0x7f06000b
+			public const int game1 = 2131099659;
 			
-			// aapt resource value: 0x7f05000c
-			public const int game2 = 2131034124;
+			// aapt resource value: 0x7f06000c
+			public const int game2 = 2131099660;
 			
-			// aapt resource value: 0x7f05000d
-			public const int game3 = 2131034125;
+			// aapt resource value: 0x7f06000d
+			public const int game3 = 2131099661;
 			
-			// aapt resource value: 0x7f05000e
-			public const int game4 = 2131034126;
+			// aapt resource value: 0x7f06000e
+			public const int game4 = 2131099662;
 			
-			// aapt resource value: 0x7f05000f
-			public const int game5 = 2131034127;
+			// aapt resource value: 0x7f06000f
+			public const int game5 = 2131099663;
 			
-			// aapt resource value: 0x7f050010
-			public const int game6 = 2131034128;
+			// aapt resource value: 0x7f060010
+			public const int game6 = 2131099664;
 			
-			// aapt resource value: 0x7f050011
-			public const int game7 = 2131034129;
+			// aapt resource value: 0x7f060011
+			public const int game7 = 2131099665;
 			
-			// aapt resource value: 0x7f050012
-			public const int game8 = 2131034130;
+			// aapt resource value: 0x7f060012
+			public const int game8 = 2131099666;
 			
-			// aapt resource value: 0x7f050013
-			public const int game9 = 2131034131;
+			// aapt resource value: 0x7f060013
+			public const int game9 = 2131099667;
 			
-			// aapt resource value: 0x7f050006
-			public const int gameArea = 2131034118;
+			// aapt resource value: 0x7f060006
+			public const int gameArea = 2131099654;
 			
-			// aapt resource value: 0x7f050005
-			public const int gameLayout = 2131034117;
+			// aapt resource value: 0x7f060005
+			public const int gameLayout = 2131099653;
 			
-			// aapt resource value: 0x7f050001
-			public const int introText = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int introText = 2131099649;
 			
-			// aapt resource value: 0x7f050008
-			public const int leftBtn = 2131034120;
+			// aapt resource value: 0x7f060008
+			public const int leftBtn = 2131099656;
 			
-			// aapt resource value: 0x7f050014
-			public const int mainLayout = 2131034132;
+			// aapt resource value: 0x7f060015
+			public const int logoImage = 2131099669;
 			
-			// aapt resource value: 0x7f05000a
-			public const int rightBtn = 2131034122;
+			// aapt resource value: 0x7f060014
+			public const int mainLayout = 2131099668;
 			
-			// aapt resource value: 0x7f050007
-			public const int scoreLbl = 2131034119;
+			// aapt resource value: 0x7f06000a
+			public const int rightBtn = 2131099658;
+			
+			// aapt resource value: 0x7f060007
+			public const int scoreLbl = 2131099655;
 			
 			static Id()
 			{
@@ -122,17 +141,17 @@ namespace PackDrop
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int ArtworkPreview = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int ArtworkPreview = 2130968576;
 			
-			// aapt resource value: 0x7f030001
-			public const int GameOver = 2130903041;
+			// aapt resource value: 0x7f040001
+			public const int GameOver = 2130968577;
 			
-			// aapt resource value: 0x7f030002
-			public const int InGame = 2130903042;
+			// aapt resource value: 0x7f040002
+			public const int InGame = 2130968578;
 			
-			// aapt resource value: 0x7f030003
-			public const int Main = 2130903043;
+			// aapt resource value: 0x7f040003
+			public const int Main = 2130968579;
 			
 			static Layout()
 			{
@@ -147,8 +166,8 @@ namespace PackDrop
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			// aapt resource value: 0x7f030000
+			public const int Icon = 2130903040;
 			
 			static Mipmap()
 			{
@@ -163,17 +182,17 @@ namespace PackDrop
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int ApplicationName = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int Hello = 2131034112;
 			
-			// aapt resource value: 0x7f040003
-			public const int app_name = 2130968579;
+			// aapt resource value: 0x7f050003
+			public const int app_name = 2131034115;
 			
-			// aapt resource value: 0x7f040002
-			public const int tap_anywhere = 2130968578;
+			// aapt resource value: 0x7f050002
+			public const int tap_anywhere = 2131034114;
 			
 			static String()
 			{
