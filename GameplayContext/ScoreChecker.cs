20170540323389341e99f6eb1187ca25037e77f7
Chris Williams <chris.williams@mixrad.io>
2016-01-20 14:48:19 +0000

    First pass of score checker

---
+++ GameplayContext/ScoreChecker.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SharedKernel;
+
+namespace GameplayContext
+{
+    public class ScoreChecker
+    {
+        public int CheckScoreAndUpdate(List<List<Tile>> columns)
+        {
+            var score = 0;
+
+            // Check vertically
+            var numberVerticalItemsInSeries = 1;
+            string lastItemChecked = null;
+            var coordsToRemove = new List<Coordinate>();
+
+            for (int i = 0; i < columns.Count;i++)
+            {
+                for (int j = 0; j < columns[i].Count;j++)
+                {
+                    if (columns[i][j].ImageId == lastItemChecked)
+                    {
+                        numberVerticalItemsInSeries++;
+                    }
+                    else
+                    {
+                        if (numberVerticalItemsInSeries > 2)
+                        {
+                            // We just finished finding a match
+                            var columnToLookAt = j == 0 ? i - 1 : i;
+                            var indexOfLastMatch = j == 0 ? columns[columnToLookAt].Count - 1 : j - 1;
+                            for (int a = indexOfLastMatch; a > (indexOfLastMatch - numberVerticalItemsInSeries); a--)
+                            {
+                                coordsToRemove.Add(new Coordinate(columnToLookAt,a));
+                            }
+
+                            score += CalculateScore(numberVerticalItemsInSeries);
+                        }
+
+                        lastItemChecked = columns[i][j].ImageId;
+                        numberVerticalItemsInSeries = 1;
+                    }
+                }
+            }
+
+            DoRemove(columns, coordsToRemove);
+
+            return score;
+        }
+
+        private int CalculateScore (int numberHorizontalItemsInSeries)
+        {
+            return 100 + ((numberHorizontalItemsInSeries - 3) * 150);
+        }
+
+        private void DoRemove (List<List<Tile>> columns, List<Coordinate> coordsToRemove)
+        {
+            foreach (var coord in coordsToRemove.OrderByDescending(x => x.Y))
+            {
+                columns[coord.X].RemoveAt(coord.Y);
+            }
+        }
+
+        private class Coordinate
+        {
+            public int X { get; private set;}
+            public int Y { get; private set;}
+
+            public Coordinate(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+    }
+}
+
---
+++ UnitTests/ScoreCheckerTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using SharedKernel;
+using GameplayContext;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class ScoreCheckerTests
+    {
+        [Test]
+        public void ThreeInAColumnIsAMatch()
+        {
+            // Arrange
+            var column1 = new List<Tile>() { Tile("2"), Tile("3"), Tile("4"), Tile("5") };
+            var column2 = new List<Tile>() { Tile("1"), Tile("2"), Tile("2"), Tile("2"), Tile("5") };
+            var column3 = new List<Tile>() { Tile("1") };
+            var columns = new List<List<Tile>>() { column1, column2, column3 };
+
+            // Act
+            var score = new ScoreChecker().CheckScoreAndUpdate(columns);
+
+            // Assert
+            Assert.AreEqual(100, score);
+            Assert.AreEqual(4, column1.Count);
+            Assert.AreEqual(1, column3.Count);
+            Assert.AreEqual(2, column2.Count);
+            Assert.AreEqual("1", column2[0].ImageId);
+            Assert.AreEqual("5", column2[1].ImageId);
+        }
+
+        [Test]
+        public void FiveInAColumnIsAMatch()
+        {
+            // Arrange
+            var column1 = new List<Tile>() { Tile("2"), Tile("3"), Tile("4"), Tile("5") };
+            var column2 = new List<Tile>() { Tile("2"), Tile("2"), Tile("2"), Tile("2"), Tile("2") };
+            var column3 = new List<Tile>() { Tile("1") };
+            var columns = new List<List<Tile>>() { column1, column2, column3 };
+
+            // Act
+            var score = new ScoreChecker().CheckScoreAndUpdate(columns);
+
+            // Assert
+            Assert.AreEqual(400, score);
+            Assert.AreEqual(4, column1.Count);
+            Assert.AreEqual(1, column3.Count);
+            Assert.AreEqual(0, column2.Count);
+        }
+
+        public Tile Tile(string imageId)
+        {
+            return new Tile { ImageId = imageId };
+        }
+    }
+}
+
